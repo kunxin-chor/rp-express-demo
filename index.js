3aecd69ad570833cfcb545d2a3a0ba14439679c7
kunxin-chor <chorkunxin@yahoo.com>
2023-09-25 16:32:01 +0000

    Added connection to database

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const ejs = require('ejs');
+const mysql = require('mysql2/promise')
+require('dotenv').config();
+
 const app = express();
+
+// initialise the database
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
 	res.render("home");
 });
+
+app.get('/recipes', async function(req,res){
+    const [results] = await pool.query('SELECT * FROM pets');
+    res.json(results);
+
+})
 
 // start server
 app.listen(8080, function(){
